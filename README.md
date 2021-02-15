0d0b3889db0816d8bb2be551bb8e5fabb1579a3b
sophiasternberg <sophiasternberg@ucsb.edu>
2021-02-15 13:59:31 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# ssaloha
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of ssaloha is to ...
+
+## Installation
+
+You can install the released version of ssaloha from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("ssaloha")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(ssaloha)
+## basic example code
+```
+
---
+++ data/and_vertebrates_doc.R
@@ -0,0 +1,11 @@
+#' Add a documentation title here
+#'
+#' Add a subtitle / brief summary here
+#'
+#' @source {Gregory, S.V. and I. Arismendi. 2020. Aquatic Vertebrate Population Study in
+#' Mack Creek, Andrews Experimental Forest, 1987 to present ver 14. Environmental Data Initiative.
+#' https://doi.org/10.6073/pasta/7c78d662e847cdbe33584add8f809165 (Accessed 2021-02-15).}
+#' \url{https://portal.edirepository.org/nis/mapbrowse?packageid=knb-lter-and.4027.14}
+
+
+"and_vertebrates"
